086d620e3721eb3a375546cf77ff2f37849f64ba
Kohaishita <79562566+Kohaishita@users.noreply.github.com>
2024-10-30 09:18:39 +0200

    Add files via upload

---
+++ lr8fordob.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <cmath> 
+using namespace std; 
+int main()
+{
+	int n,i,k;
+	double y,Z, dob = 1.0;
+	cout << "Enter n= ";
+	cin >> n;
+	cout << "Enter y= ";
+	cin >> y;
+	for (i = 1; i <= n; ++i) {
+		for (k = 1; k <= i; ++k) {
+			dob *= k * k;
+		}
+		Z = (y + dob * y) / (y * y + pow(sin(y), 2));
+		cout << "k=" << i << "||" << "Z= " << Z << endl;
+	}
+	return 0;
+}
+
